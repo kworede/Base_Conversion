a6ee20acd5967eea74ad7203f5fa57e6658a4685
kworede <37035048+kworede@users.noreply.github.com>
2018-03-04 16:36:32 -0800

    Add files via upload

---
+++ Convert_Binary.c
@@ -0,0 +1,44 @@
+//Kirubel Worede
+//HW #0
+// 01/14/2016
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+const char num_key[16] = "0123456789abcdef";
+void ConvertBinary(int num);
+void Convert(int num, int num_base);
+
+int main(int argc, char *argv[]) {
+  if (argc == 2){
+     int num = atoi(argv[1]);
+      ConvertBinary(num);
+     }
+  else if (argc >2) {
+      int num = atoi(argv[1]);
+      int num_base = atoi(argv[2]);
+      Convert(num, num_base);
+      }
+ return 0;
+ }
+void ConvertBinary(int num) {
+  char number [256];
+  number[0] = 0;
+  do {
+    strcpy (number+1, number);
+    number[0]= num_key[num % 2];
+    num = num/2;
+  }while (num !=0);
+ printf("your number in Binary is: %s\n", number);
+}
+
+  void Convert(int num, int num_base) {
+    char number [256];
+  number[0] = 0;
+  do {
+    strcpy (number+1, number);
+    number[0]= num_key[num % num_base];
+    num = num/num_base;
+  } while (num !=0);
+  printf("your number in base %i is: %s\n", num_base, number);
+  }
+
